fc73d2af832f8909e94afeace8dcd14ca84c083a
Ll <dididy@knou.ac.kr>
2018-08-22 14:15:14 +0900

    Last fix for server_.py

--- server_.py
+++ server_.py
@@ -5,6 +5,7 @@ import sys
 import time
 from pyfirmata import Arduino, util
 import time
+import serial
 
 print "Searching for devices..."
 print ""
@@ -32,52 +33,38 @@ default = 'feh -F --auto-zoom img/default.png &'
 kill = 'pkill -9 -ef feh'
 count = 0
 
-board = Arduino('/dev/ttyACM0')
 
-it = util.Iterator(board)
-it.start()
-
-joy_x = board.get_pin('a:1:p')# analog/digital:pin_num:input/output 
-joy_y = board.get_pin('a:0:p')
-joy_push = board.get_pin('d:3:i')
-
-joy_x.enable_reporting()
-joy_y.enable_reporting()
-joy_push.enable_reporting()
+ser = serial.Serial('/dev/ttyACM0',9600, timeout = 1)
 
 while 1:
-  x = joy_x.read()
-  y = joy_y.read()
-  push = joy_push.read()
-  
-  print(x)
-  print(y)
-  print(push)
-  
-  why = sock.recv(1024)
+  why=ser.read(1)
   if(why == '1'):
     os.system(cmd1)
     while(why == '1'):
-      why = sock.recv(1024)
-      time.sleep(0.01)
+      why=ser.readline(1)
+      sock.sendall('1')
+      time.sleep(0.005)
+    sock.sendall('9')
     os.system(default)
-    print("ln")
     
   elif(why == '2'):
     os.system(cmd2)
     while(why == '2'):
-      why = sock.recv(1024)
-      time.sleep(0.01)
+      why=ser.readline(1)
+      sock.sendall('2')
+      time.sleep(0.005)
+    sock.sendall('9')
     os.system(default)
-    print("lu")
 
   elif(why == '3'):
     os.system(smile)
     while(why == '3'):
-      why = sock.recv(1024)
-      time.sleep(0.01)
+      why=ser.readline(1)
+      sock.sendall('3')
+      time.sleep(0.005)
+    sock.sendall('9')
     os.system(default)
  
   elif(why == '4'):
+    sock.sendall('4')
     os.system(kill)
-###https://m.blog.naver.com/PostView.nhn?blogId=cosmosjs&logNo=220805719737&proxyReferer=https%3A%2F%2Fwww.google.co.kr%2F
