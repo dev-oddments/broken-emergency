9cd956c117aa548d7f0b506c6b568e36a6e0f124
Yongjae Yi <dididy@knou.ac.kr>
2019-04-05 08:46:35 +0900

    Update _NEW_SafePIXEL.ino

--- _NEW_SafePIXEL.ino
+++ _NEW_SafePIXEL.ino
@@ -1,6 +1,3 @@
-// 8x8 generator 
-// http://www.pial.net/8x8-dot-matrix-font-generator-based-on-javascript-and-html/
-// http://robojax.com/learn/arduino/8x8LED/
 #include <Wire.h>
 #include "Adafruit_LEDBackpack.h"
 #include "Adafruit_GFX.h"
@@ -19,14 +16,16 @@ void setup() {
     matrix[m].begin(0x70 + m);
     matrix[m].clear();
     matrix[m].setTextSize(1);
-    matrix[m].setTextWrap(false);
+    matrix[m].setTextWrap(false);  // we dont want text to wrap so it scrolls nicely
+    matrix[m].setRotation(1);
+
   }
   
 }
 
 static const uint8_t PROGMEM
-  default_sign[][24] = {
-  { 0B11111111, 0B11111111, 0B11111111,
+  default_sign[][24] = {                 // Mouth animation frames
+  { 0B11111111, 0B11111111, 0B11111111, // Mouth position A
     0B10000000, 0B00000000, 0B00000001,
     0B10111111, 0B11111111, 0B11111101,
     0B10100000, 0B00000000, 0B00000101,
@@ -34,7 +33,7 @@ static const uint8_t PROGMEM
     0B10111111, 0B11111111, 0B11111101,
     0B10000000, 0B00000000, 0B00000001,
     0B11111111, 0B11111111, 0B11111111 } },
-  left_sign[][8] = { 
+  left_sign[][8] = {    // Eye animation frames
   {
     B00011000,
     B00110001,
@@ -82,7 +81,7 @@ static const uint8_t PROGMEM
     B10001100
   }, },
   
-  right_sign[][8] = {
+  right_sign[][8] = {    // Eye animation frames
   {
     B00011000,
     B10001100,
@@ -129,7 +128,7 @@ static const uint8_t PROGMEM
     B00011000,
     B00110001
   } },
-  progress_sign[][8] = {
+  progress_sign[][8] = {    // Eye animation frames
   {
     B11111111,
     B10000001,
@@ -158,7 +157,7 @@ static const uint8_t PROGMEM
     B01111110,
     B10000001
   } },
-  alert_sign[][24] = {
+  alert_sign[][24] = {    // Eye animation frames
   { 
     0B00011000, 0B00000000, 0B00000000,
     0B00011000, 0B01000010, 0B01000010,
@@ -201,9 +200,8 @@ void loop() {
   if(boot_flag == 0){
     boot();
   }
-  int joy_x = analogRead(JOY_X);                      
-  int joy_y = analogRead(JOY_Y);
-  Serial.write(joy_x);
+  int joy_x = analogRead(JOY_X);                           // 변수 X에 아날로그 1번핀에 입력되는 신호를 대입
+  int joy_y = analogRead(JOY_Y);                           // 변수 Y에 아날로그 0번핀에 입력되는 신호를 대입
 
   if(joy_y < 100) { // left
     leftSign();
@@ -217,6 +215,9 @@ void loop() {
   if(joy_x < 100) {
     alertSign();
   }
+//  leftSign();
+//  delay(500);
+//  delay(500);
 }
 
 void scroll(char* text) {
@@ -225,17 +226,15 @@ void scroll(char* text) {
   int joy_y = analogRead(JOY_Y);
   for (int x=scrollPositions / 2; x>=-scrollPositions; x--) {
    for(uint8_t m=0; m<NUM_MATRICES; m++) {
-    delay(120);
-    if(joy_x < 400 || joy_x > 600 || joy_y < 400 || joy_y > 600){break;}
-    joy_x = analogRead(JOY_X); 
-    joy_y = analogRead(JOY_Y);      
-     
     matrix[m].clear();
     matrix[m].setCursor((x - (m * 8)), 0);
     matrix[m].print(text);
     matrix[m].writeDisplay();
+    joy_x = analogRead(JOY_X); 
+    joy_y = analogRead(JOY_Y); 
+    if(joy_x < 400 || joy_x > 600 || joy_y < 400 || joy_y > 600){break;}     
    }
-
+   delay(30);
   }
 
 }
@@ -254,6 +253,8 @@ void defaultSign() {
     matrix[i].writeDisplay();
   }
 }
+
+
 
 void leftSign() {
   for(int x = 0; x < 5; x++) {
@@ -289,7 +290,8 @@ void alertSign() {
   for(int x = 0; x < 4; x++){
     delay(400);
     for(uint8_t i=0; i<3; i++) {
-      matrix[i].clear();i * -8, 0, alert_sign[x], 24, 8, LED_ON);
+      matrix[i].clear();
+      matrix[i].drawBitmap(i * -8, 0, alert_sign[x], 24, 8, LED_ON);
       matrix[i].writeDisplay();
     }
   }
